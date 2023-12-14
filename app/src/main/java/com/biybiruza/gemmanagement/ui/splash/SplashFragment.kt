9bcd631a675babb35e0df41b312e3fbdc480fb89
dalloris <dalloris.uz@gmail.com>
2023-12-14 19:44:33 +0500

    Next changes

--- app/src/main/java/com/biybiruza/gemmanagement/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/biybiruza/gemmanagement/ui/splash/SplashFragment.kt
@@ -4,19 +4,26 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.biybiruza.gemmanagement.R
+import com.biybiruza.gemmanagement.databinding.FragmentHomeBinding
 import com.biybiruza.gemmanagement.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
-
-    private lateinit var binding: FragmentSplashBinding
-
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentSplashBinding.bind(view)
+        _binding = FragmentSplashBinding.bind(view)
 
         Handler().postDelayed({
             binding.progress.visibility = View.GONE
+            findNavController().navigate(R.id.action_splashFragment_to_signInFragment)
         },3000)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
     }
 }
